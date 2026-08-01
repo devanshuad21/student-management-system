0b3968a1298ee9a9d49d020701dd6a3b0ba3bcb0
Devanshu Jain <devjain@Devanshus-MacBook-Air.local>
2026-08-01 21:05:50 +0530

    Initial Project Setup

---
+++ src/main/java/com/dev/Main.java
@@ -0,0 +1,10 @@
+package com.dev;
+
+import static java.lang.IO.*;
+import static java.lang.String.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+}
